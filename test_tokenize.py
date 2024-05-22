a8f462208c2415094d8b191b3be2ed30b3c9ad8f
Patrick Elmer <patrick@elmer.ws>
2024-05-23 05:53:37 +0900

    Remove benchmark from test

--- test_tokenize.py
+++ test_tokenize.py
@@ -1,5 +1,5 @@
 from tokenize import tokenize
 
 
-def test_tokenize(benchmark):
-    assert benchmark(tokenize, 'at͡suːi') == ('a', 't͡s', 'uː', 'i')
+def test_tokenize():
+    assert tokenize('at͡suːi') == ('a', 't͡s', 'uː', 'i')
