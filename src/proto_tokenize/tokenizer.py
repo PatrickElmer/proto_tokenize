acb4847e550a68e9d3abb3a34eb56d00e2c556c3
Patrick Elmer <patrick@elmer.ws>
2025-11-30 16:45:25 +0100

    Refactor code

--- src/proto_tokenize/tokenizer.py
+++ src/proto_tokenize/tokenizer.py
@@ -8,10 +8,12 @@ def tokenize(
     if not word:
         return []
 
-    bounds = [0] + [
-        i for i in range(1, len(word))
-        if word[i] not in combiners | modifiers
-        and word[i - 1] not in combiners | stresses
-    ] + [len(word)]
+    def is_boundary(i):
+        return (
+            word[i] not in combiners | modifiers
+            and word[i - 1] not in stresses | combiners
+        )
 
-    return [word[bounds[i]:bounds[i + 1]] for i in range(len(bounds) - 1)]
+    bounds = [0, *filter(is_boundary, range(1, len(word))), len(word)]
+
+    return [word[bounds[i] : bounds[i + 1]] for i in range(len(bounds) - 1)]
