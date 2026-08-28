e8f460837c2f6390c4ac81d8aebad76fa205f6bb
Patrick Elmer <patrick@elmer.ws>
2026-08-28 23:30:17 +0200

    Fix tests

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -5,43 +5,56 @@ import pytest
 def test_empty_string():
     assert tokenize("") == []
 
+
 def test_single_char():
     assert tokenize("a") == ["a"]
+
 
 def test_short_word():
     assert tokenize("ab") == ["a", "b"]
 
+
 def test_modifier():
     assert tokenize("aː") == ["aː"]
+
 
 def test_combiner():
     assert tokenize("t͡s") == ["t͡s"]
 
+
 def test_word_with_modifier():
     assert tokenize("maːm") == ["m", "aː", "m"]
+
 
 def test_word_with_combiner():
     assert tokenize("at͡sa") == ["a", "t͡s", "a"]
 
+
 def test_prenasalized():
     assert tokenize("aᵐ͡ba") == ["a", "ᵐ͡b", "a"]
 
+
 def test_wrong_prenasalization_if_nasal_in_modifiers():
     assert tokenize("aⁿ͡ta") == ["aⁿ͡t", "a"]
-    assert tokenize("aⁿ͡ta", modifiers={""}) == ["a", "ⁿ͡t", "a"]
+    assert tokenize("aⁿ͡ta", diacritics=set("͜͡‿")) == ["a", "ⁿ͡t", "a"]
+
 
 def test_word_with_combiner_and_modifier():
     assert tokenize("at͡suːi") == ["a", "t͡s", "uː", "i"]
 
+
 def test_custom_modifier():
-    assert tokenize("ts", modifiers={"s"}) == ["ts"]
+    assert tokenize("ts", diacritics={"s"}) == ["ts"]
+
 
 def test_custom_combiner():
-    assert tokenize("a-b", combiners={"-"}) == ["a-b"]
+    assert tokenize("a-b", diacritics={"-"}, proclitics={"-"}) == ["a-b"]
+
 
 def test_missing_argument():
     with pytest.raises(TypeError):
         tokenize()
+
 
 def test_stress():
     assert tokenize("ˈaˌb") == ["ˈa", "ˌb"]
