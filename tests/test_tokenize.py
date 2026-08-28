c56cb0e971deaba6727eaa560f841487846cfae5
Patrick Elmer <patrick@elmer.ws>
2026-08-29 00:05:45 +0200

    Adjust tests to new implementation and add new test cases

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -1,5 +1,6 @@
-from tokenizer import tokenize
 import pytest
+
+from tokenizer import tokenize
 
 
 def test_empty_string():
@@ -14,41 +15,57 @@ def test_short_word():
     assert tokenize("ab") == ["a", "b"]
 
 
-def test_modifier():
+def test_diacritic():
     assert tokenize("aː") == ["aː"]
 
 
-def test_combiner():
+def test_tie_bar():
     assert tokenize("t͡s") == ["t͡s"]
 
 
-def test_word_with_modifier():
+def test_word_with_diacritic():
     assert tokenize("maːm") == ["m", "aː", "m"]
 
 
-def test_word_with_combiner():
+def test_word_with_tie_bar():
     assert tokenize("at͡sa") == ["a", "t͡s", "a"]
+
+
+def test_word_with_tie_bar_and_diacritic():
+    assert tokenize("at͡suːi") == ["a", "t͡s", "uː", "i"]
+
+
+def test_stress():
+    assert tokenize("ˈaˌb") == ["ˈa", "ˌb"]
 
 
 def test_prenasalized():
     assert tokenize("aᵐ͡ba") == ["a", "ᵐ͡b", "a"]
 
 
-def test_wrong_prenasalization_if_nasal_in_modifiers():
+def test_wrong_prenasalization_if_nasal_is_a_diacritic():
     assert tokenize("aⁿ͡ta") == ["aⁿ͡t", "a"]
-    assert tokenize("aⁿ͡ta", diacritics=set("͜͡‿")) == ["a", "ⁿ͡t", "a"]
+    assert tokenize("aⁿ͡ta", diacritics="͡") == ["a", "ⁿ͡t", "a"]
 
 
-def test_word_with_combiner_and_modifier():
-    assert tokenize("at͡suːi") == ["a", "t͡s", "uː", "i"]
+def test_custom_diacritic():
+    assert tokenize("ts", diacritics="s") == ["ts"]
 
 
-def test_custom_modifier():
+def test_custom_tie_bar():
+    assert tokenize("a-b", diacritics="-", proclitics="-") == ["a-b"]
+
+
+def test_tie_bar_must_be_in_both_arguments_to_bind_both_ways():
+    assert tokenize("a-b", diacritics="-") == ["a-", "b"]
+    assert tokenize("a-b", proclitics="-") == ["a", "-b"]
+
+
+def test_any_container_of_characters_is_accepted():
+    assert tokenize("ts", diacritics="s") == ["ts"]
     assert tokenize("ts", diacritics={"s"}) == ["ts"]
-
-
-def test_custom_combiner():
-    assert tokenize("a-b", diacritics={"-"}, proclitics={"-"}) == ["a-b"]
+    assert tokenize("ts", diacritics=["s"]) == ["ts"]
+    assert tokenize("ts", diacritics=frozenset("s")) == ["ts"]
 
 
 def test_missing_argument():
@@ -56,5 +73,25 @@ def test_missing_argument():
         tokenize()
 
 
-def test_stress():
-    assert tokenize("ˈaˌb") == ["ˈa", "ˌb"]
+def test_trailing_tie_bar():
+    assert tokenize("at͡") == ["a", "t͡"]
+
+
+def test_leading_tie_bar():
+    assert tokenize("͡ab") == ["͡a", "b"]
+
+
+def test_leading_diacritic():
+    assert tokenize("ːa") == ["ː", "a"]
+
+
+def test_trailing_stress():
+    assert tokenize("aˈ") == ["a", "ˈ"]
+
+
+def test_lone_stress():
+    assert tokenize("ˈ") == ["ˈ"]
+
+
+def test_doubled_stress():
+    assert tokenize("ˈˈa") == ["ˈˈa"]
